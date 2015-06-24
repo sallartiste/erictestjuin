4f475b7e586bece2933b3a8bd2a8cad5aaa47d04
Eric Salla <sallartistee@yahoo.fr>
2015-06-24 10:49:48 +0200

    Mise en page du home  et ajout css 23/06/2015

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Telepro-photo</title>
+        <link rel="stylesheet" href="styles.css" type="text/css" />
+    </head>
+
+    <body>
+       <div class="wrap">
+         <header>
+          <h1>Bienvenu sur Telepro-photo.fr</h1>
+          <nav>
+              <ul>
+                 <li><a href="">Accueil</a></li>
+                <li><a href="">Catégories</a>
+                  <ul>
+                       <li><a href="">Animaux</a></li>
+                       <li><a href="">Architectures</a></li>
+                       <li><a href="">Artistiques</a></li>
+                       <li><a href="">Personnes</a></li>
+                       <li><a href="">Paysages</a></li>
+                       <li><a href="">Sports</a></li>
+                       <li><a href="">technologies</a></li>
+                       <li><a href="">Transports</a></li>
+                       <li><a href="">Divers</a></li>
+                   </ul>
+                 </li>
+                 <li><a href="">Nous Contacter</a></li>
+                 <li><a href="">Espaces Clients</a></li>
+                 <li><a href="">Connexion</a></li>
+              </ul>
+          </nav>
+        </header>
+       </div>
+    </body>
+</html>
